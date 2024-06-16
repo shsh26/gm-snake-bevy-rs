d0b241f782257996fe1d96ad829c006462970599
shsh26 <sagong126@gmail.com>
2024-06-17 01:24:20 +0900

    :sparkles: feat: 크기, 위치 후처리 추가

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,30 @@
 use bevy::prelude::*;
+use bevy::window::PrimaryWindow;
 
 const SNAKE_HEAD_COLOR: Color = Color::rgb(0.7, 0.7, 0.7);
+
+const ARENA_WIDTH: u32 = 10;
+const ARENA_HEIGHT: u32 = 10;
+
+#[derive(Component, Clone, Copy, PartialEq, Eq)]
+struct Position {
+    x: i32,
+    y: i32,
+}
+
+#[derive(Component)]
+struct Size {
+    width: f32,
+    height: f32,
+}
+impl Size {
+    pub fn square(x: f32) -> Self {
+        Self {
+            width: x,
+            height: x,
+        }
+    }
+}
 
 #[derive(Component)]
 struct SnakeHead;
@@ -22,25 +46,52 @@ fn spawn_snake(mut commands: Commands) {
         },
         ..default()
     })
-        .insert(SnakeHead);
+        .insert(SnakeHead)
+        .insert(Position { x: 3, y: 3 })
+        .insert(Size::square(0.8));
 }
 
-fn snake_movement(keyboard_input: Res<ButtonInput<KeyCode>>, mut head_positions: Query<(&SnakeHead, &mut Transform)>) {
-    for (_, mut transform) in head_positions.iter_mut() {
+fn snake_movement(keyboard_input: Res<ButtonInput<KeyCode>>, mut head_positions: Query<&mut Position, With<SnakeHead>>) {
+    for mut pos in head_positions.iter_mut() {
         if keyboard_input.pressed(KeyCode::ArrowUp) {
-            transform.translation.y += 2.0;
+            pos.y += 1;
         }
         if keyboard_input.pressed(KeyCode::ArrowDown) {
-            transform.translation.y -= 2.0;
+            pos.y -= 1;
         }
         if keyboard_input.pressed(KeyCode::ArrowLeft) {
-            transform.translation.x -= 2.0;
+            pos.x -= 1;
         }
         if keyboard_input.pressed(KeyCode::ArrowRight) {
-            transform.translation.x += 2.0;
+            pos.x += 1;
         }
     }
-    
+}
+
+fn size_scaling(windows: Query<&Window, With<PrimaryWindow>>, mut q: Query<(&Size, &mut Transform)>) {
+    let window = windows.get_single().unwrap();
+    for (size, mut transform) in q.iter_mut() {
+        transform.scale = Vec3::new(
+            size.width / ARENA_WIDTH as f32 * window.width() as f32,
+            size.height / ARENA_HEIGHT as f32 * window.height() as f32,
+            1.0,
+        );
+    }
+}
+
+fn position_translation(windows: Query<&Window, With<PrimaryWindow>>, mut q: Query<(&Position, &mut Transform)>) {
+    fn convert(pos: f32, bound_window: f32, bound_game: f32) -> f32 {
+        let tile_size = bound_window / bound_game;
+        pos / bound_game * bound_window - (bound_window / 2.0) + (tile_size / 2.0)
+    }
+    let window = windows.get_single().unwrap();
+    for (pos, mut transform) in q.iter_mut() {
+        transform.translation = Vec3::new(
+            convert(pos.x as f32, window.width() as f32, ARENA_WIDTH as f32),
+            convert(pos.y as f32, window.height() as f32, ARENA_HEIGHT as f32),
+            0.0,
+        );
+    }
 }
 
 fn main() {
@@ -49,5 +100,7 @@ fn main() {
         // 프로그램 시작 시 카메라 설정 함수 실행
         .add_systems(Startup, (setup_camera, spawn_snake))
         .add_systems(Update, snake_movement)
+        // 기본 세팅 후 크기, 위치 변환
+        .add_systems(PostUpdate, (position_translation, size_scaling))
         .run();
 }
