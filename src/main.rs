bb8bb01b84b19a9055fe4bebaf82a002eb306bc3
shsh26 <sagong126@gmail.com>
2024-06-16 19:28:01 +0900

    :sparkles: feat: 기본 뱀 머리 추가

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,28 @@
 use bevy::prelude::*;
 
+const SNAKE_HEAD_COLOR: Color = Color::rgb(0.7, 0.7, 0.7);
+
+#[derive(Component)]
+struct SnakeHead;
+
 fn setup_camera(mut commands: Commands) {
     // commands.spawn_bundle(OrthographicCameraBundle::new_2d());
     commands.spawn(Camera2dBundle::default());
+}
+
+fn spawn_snake(mut commands: Commands) {
+    commands.spawn(SpriteBundle {
+        sprite: Sprite {
+            color: SNAKE_HEAD_COLOR,
+            ..default()
+        },
+        transform: Transform {
+            scale: Vec3::new(10.0, 10.0, 10.0),
+            ..default()
+        },
+        ..default()
+    })
+        .insert(SnakeHead);
 }
 
 fn main() {
@@ -10,5 +30,6 @@ fn main() {
         .add_plugins(DefaultPlugins)
         // 프로그램 시작 시 카메라 설정 함수 실행
         .add_systems(Startup, setup_camera)
+        .add_systems(Startup, spawn_snake)
         .run();
 }
